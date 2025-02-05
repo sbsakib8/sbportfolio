19c76fe4cfc8fdd9121464237e6c72e9d7f3740e
sakib <sbsakibsarkar8@gmail.com>
2025-02-06 03:31:52 +0600

    add page file

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,9 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import Navber from "@/component/navber/Navber";
 
 export default function Home() {
   return (
     <div>
-       <h1 className="text-3xl text-red-500 font-bold underline">
-      Hello world! fff
-    </h1>
-    <h2 className=" text-green-600 text-[30px]/40">sb sakib sarkar</h2>
+      <Navber/>
     
     </div>
   );
