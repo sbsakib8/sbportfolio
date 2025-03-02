66fcf4ee031886831f35ed971bd8ebf7f4ed00be
sakib <sbsakibsarkar8@gmail.com>
2025-03-03 02:06:06 +0600

    about done

--- src/app/(page)/about/page.js
+++ src/app/(page)/about/page.js
@@ -1,4 +1,5 @@
 import AboutTyping from "@/component/about/AboutTyping"
+import MediaAndButton from "@/component/about/MediaAndButton"
 
 function About() {
 
@@ -11,11 +12,14 @@ function About() {
       <AboutTyping/>
       </h3>
       <p className="text-[18px] text-wrap md:pr-6 lg:mt-18">I have been working in the field of web development since 2023. My passion for programming started when I was a kid. I love to think about the future, and I think programming is the best option in the future. I don't know if my coding is better than everyone else's. But I try my best to write good coding so that everyone will fall in love with my coding. I am now in Dubai. I am working in hotel.</p>
+
+      <MediaAndButton/>
      </div>
 
      <div className="md:w-[50%] flex flex-col justify-center items-center md:-mt-5">
            <img className=" w-[450px]" src='mypic/about.png'></img>
       </div>
+
     </div>
     
   )
--- src/app/(page)/home/page.js
+++ src/app/(page)/home/page.js
@@ -3,6 +3,7 @@ import mypic from "../../../../public/mypic/home.png"
 import Media from '@/component/navber/Media'
 import Link from 'next/link'
 import HomeTyping from '@/component/home/HomeTyping'
+import MediaAndButton from '@/component/home/MediaAndButton'
 
 function Home() {
   
@@ -19,13 +20,7 @@ function Home() {
         </div>
         <p className='home_paragraph'>I am Shakib. front end developer. I have been doing web development since 2023.The reason for coming to programming is, actually I like coding a lot, and thinking about the future, I think programming is the best option in the future.I don't know if my coding is better than everyone else's. But I try my best to write good coding so that everyone will fall in love with my coding.I am now in Dubai. I am working in hotel and also doing programming and learning new things.Being a Bengali, working alone was very difficult for me. Now I want a front end development job.Pray for me, that I can do something good…</p>
          
-         <div>
-          <Media/>
-         </div>
-        
-        <div className=' flex justify-center md:justify-start' >
-        <button type="button" className='home_button'><Link href='/contact' >Contact</Link></button>
-        </div>
+         <MediaAndButton/>
         
       </div>
       <div className="home-img">
