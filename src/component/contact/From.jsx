fef1a232266fa6dd83f7e1eb9c15fb48d3d03175
sakib <sbsakibsarkar8@gmail.com>
2025-03-14 02:31:27 +0600

    form

--- src/component/contact/From.jsx
+++ src/component/contact/From.jsx
@@ -1,25 +1,95 @@
-"use Client"
+"use client"
+
+import { useFormik } from 'formik';
+import * as yup from 'yup';
+
 
 function From() {
+
+    const formik = useFormik({
+        initialValues: {
+          name: '',
+          email:'',
+          message:'',
+          Subject:'',
+        },
+        validationSchema: yup.object({
+          name: yup.string()
+             .min(2, 'Name must be at least 2 characters')
+             .max(50, 'Name must not exceed 50 characters')
+             .required('Name is required'),
+          email: yup.string()
+             .email('Invalid email address')
+             .required('Email is required'),
+          message: yup.string()
+             .min(10, 'Message must be at least 10 characters')
+             .max(1000, 'Message must not exceed 1000 characters')
+             .required('Message is required'),
+          Subject: yup.string()
+             .min(2, 'Subject must be at least 2 characters')
+             .max(100, 'Subject must not exceed 100 characters')
+             .required('Subject is required'),
+        }),
+        onSubmit: (values ) => {
+          console.log(values)
+          formik.resetForm({
+            values: { name: '', email: '' , message: '' , Subject: ''  },
+           
+          });
+        },
+      });
+    
     return (
-        <div className='py-10 bg-background w-full'>
-            <fieldset className="fieldset w-full bg-base-200 border border-base-300 p-4 rounded-box">
+        <div className='py-10 lg:pt-0 lg:-mt-5 bg-background w-full flex flex-col justify-center items-center '>
+           <form  action=""
+         method="POST" className='w-full lg:ml-26' onSubmit={formik.handleSubmit}>
+           <fieldset className="fieldset w-full bg-base-200 border border-base-300 p-4 rounded-box">
                 
-               <div className="flex flex-col lg:flex-row gap-5 md:gap-10">
-              
-                <input type="text" className="input w-full md:w-1/2 text-[18px] " placeholder="Name" />
+                <div className="flex flex-col lg:flex-row gap-5 md:gap-10">
+               
+                 <input  onChange={formik.handleChange} id="name"
+         name="name" value={formik.values.name} type="text" className="input w-full md:w-1/2 text-[18px] " placeholder="Name" />
+        
+                 
+                 <input onChange={formik.handleChange} id="email"
+         name="email" value={formik.values.email} type="email" className="input w-full md:w-1/2 text-[16px]" placeholder="Email" />
+            
+                </div>
+                <br />
+                <div className=' flex justify-between gap-7'>
+                {
+                  formik.touched.name && formik.errors.name? (
+                    <p className="text-red-500 ">{formik.errors.name}</p>
+                  ) : null
+                }
+               
+                {
+                  formik.touched.email && formik.errors.email? (
+                    <p className="text-red-500">{formik.errors.email}</p>
+                  ) : null
+                }
+                </div>
 
-                
-                <input type="email" className="input w-full md:w-1/2 text-[16px]" placeholder="Email" />
-
-               </div>
-               <input type="text" className="input w-full mt-5 text-[24px] font-bold" placeholder="Subject" />
-                
-                <textarea className="textarea h-34 w-full mt-5 text-[17px]" placeholder="Bio"></textarea>
- 
-
-               <button className="btn btn-neutral mt-4 w-[200px] rounded-full bg-primarys text-black text-[18px] font-bold hover:shadow-lgs">Send Message</button>
-            </fieldset>
+                <input onChange={formik.handleChange} id="Subject"
+         name="Subject" value={formik.values.Subject} type="text" className="input w-full  text-[24px] font-bold" placeholder="Subject" />
+                 <br/>
+                 {
+                  formik.touched.Subject && formik.errors.Subject? (
+                    <p className="text-red-500">{formik.errors.Subject}</p>
+                  ) : null
+                }
+                 <textarea onChange={formik.handleChange} id="message"
+         name="message" value={formik.values.message} className="textarea h-34 w-full  text-[17px]" placeholder="Bio"></textarea>
+  
+                   <br/>
+                   {
+                  formik.touched.message && formik.errors.message? (
+                    <p className="text-red-500 ">{formik.errors.message}</p>
+                  ) : null
+                }
+                <button type="submit" className="btn btn-neutral mt-4 w-[200px] rounded-full bg-primarys text-black text-[18px] font-bold hover:shadow-lgs">Send Message</button>
+             </fieldset>
+           </form>
         </div>
     )
 }
